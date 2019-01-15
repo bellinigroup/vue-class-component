47c63383217d5d6d1d68dc67fd44da5174b6e68b
mrnateriver <mr.nate.river@gmail.com>
2019-01-15 15:55:13 +0700

    updated build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,9 +3,12 @@ import { VueClass } from './declarations';
 import { componentFactory } from './component';
 export { createDecorator, VueDecorator, mixins } from './util';
 declare function Component<V extends Vue>(options: ComponentOptions<V> & ThisType<V>): <VC extends VueClass<V>>(target: VC) => VC;
+declare namespace Component {
+    var registerHooks: (keys: string[]) => void;
+}
 declare function Component<VC extends VueClass<Vue>>(target: VC): VC;
 declare namespace Component {
-    function registerHooks(keys: string[]): void;
+    var registerHooks: (keys: string[]) => void;
 }
 export default Component;
 export { componentFactory, VueClass };
